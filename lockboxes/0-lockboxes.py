192e23f1a2219d83a80272c8af88c46b985474a6
davisjoseph6 <davisjoseph767@email.com>
2024-05-19 02:23:20 +0200

    updated

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
