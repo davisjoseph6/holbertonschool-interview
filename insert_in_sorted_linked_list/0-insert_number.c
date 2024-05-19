97f6a874a27a7d91a7b68874c15140781cd0e0ea
davisjoseph6 <davisjoseph767@email.com>
2024-05-19 04:11:15 +0200

    updated

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -10,32 +10,31 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node;
-    listint_t *current;
+	listint_t *new_node;
+	listint_t *current;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = number;
-    new_node->next = NULL;
+	new_node->n = number;
+	new_node->next = NULL;
 
-    if (*head == NULL || (*head)->n >= number)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL || (*head)->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    current = *head;
-    while (current->next != NULL && current->next->n < number)
-    {
-        current = current->next;
-    }
+	current = *head;
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
 
-    new_node->next = current->next;
-    current->next = new_node;
+	new_node->next = current->next;
+	current->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
-
