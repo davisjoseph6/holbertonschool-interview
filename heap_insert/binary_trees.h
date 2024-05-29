3599b814c66d3d67ba8408e42626bb6798af6485
davisjoseph6 <davisjoseph767@email.com>
2024-05-29 04:00:21 +0200

    wrote a function that creates a binary tree node

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,8 +1,6 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -13,21 +11,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
-
-
-/* Functions */
+} binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* _BINARY_TREES_H_ */
