1238b3d773bf7da291d5051648cf78c6a7949cf7
davisjoseph6 <davisjoseph767@email.com>
2024-06-12 04:06:50 +0200

    fixed betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,71 +1,110 @@
 #include <stdio.h>
 #include "sandpiles.h"
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
-static int is_stable(int grid[3][3]);
-static void topple(int grid[3][3]);
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: First 3x3 grid (will contain the result)
+ * @grid2: Second 3x3 grid
+ */
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
 	int i, j;
 	/* Add grid2 to grid1 */
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
 			grid1[i][j] += grid2[i][j];
 		}
 	}
 
 	/* Topple until stable */
-	while (!is_stable(grid1)) {
+	while (!is_stable(grid1))
+	{
 		printf("=\n");
 		print_grid(grid1);
 		topple(grid1);
 	}
 }
 
-static void print_grid(int grid[3][3]) {
+/**
+ * print_grid - Prints a 3x3 grid
+ * @grid: 3x3 grid to print
+ */
+static void print_grid(int grid[3][3])
+{
 	int i, j;
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (j) printf(" ");
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-static int is_stable(int grid[3][3]) {
+/**
+ * is_stable - Checks if a sandpile is stable
+ * @grid: 3x3 grid to check
+ *
+ * Return: 1 if stable, 0 otherwise
+ */
+static int is_stable(int grid[3][3])
+{
 	int i, j;
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (grid[i][j] > 3) {
-				return 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				return (0);
 			}
 		}
 	}
-	return 1;
+	return (1);
 }
 
-static void topple(int grid[3][3]) {
+/**
+ * topple - Topples a sandpile
+ * @grid: 3x3 grid to topple
+ */
+static void topple(int grid[3][3])
+{
 	int i, j;
 	int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
 	/* Distribute grains */
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (grid[i][j] > 3) {
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
 				temp[i][j] -= 4;
-				if (i > 0) temp[i - 1][j]++;
-				if (i < 2) temp[i + 1][j]++;
-				if (j > 0) temp[i][j - 1]++;
-				if (j < 2) temp[i][j + 1]++;
+				if (i > 0)
+					temp[i - 1][j]++;
+				if (i < 2)
+					temp[i + 1][j]++;
+				if (j > 0)
+					temp[i][j - 1]++;
+				if (j < 2)
+					temp[i][j + 1]++;
 			}
 		}
 	}
 
 	/* Update grid with the new values */
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
 			grid[i][j] += temp[i][j];
 		}
 	}
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,5 +2,9 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+static int is_stable(int grid[3][3]);
+static void topple(int grid[3][3]);
+			
 
 #endif /* SANDPILES_H */
