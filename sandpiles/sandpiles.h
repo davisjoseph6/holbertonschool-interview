0fd5b5f44c8c58118949f4d15c1dc993d5e0e392
davisjoseph6 <davisjoseph767@email.com>
2024-06-12 04:18:28 +0200

    fixed betty

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,6 +5,6 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 int is_stable(int grid[3][3]);
 void topple(int grid[3][3]);
-			
+
 
 #endif /* SANDPILES_H */
