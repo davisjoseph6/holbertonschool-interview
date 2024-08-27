62ca9590e26683843e208e3ebe50984ba43738eb
davisjoseph6 <davisjoseph767@email.com>
2024-08-27 05:53:36 +0200

    updated

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,41 +2,41 @@
 
 const request = require('request');
 
-//Get the Movie ID from the first positional argument
+// Get the Movie ID from the first positional argument
 const movieId = process.argv[2];
 
-//Define the URL for the Star Wars API for the specific movie
+// Define the URL for the Star Wars API for the specific movie
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-		return;
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	const filmData = JSON.parse(body);
-	const characters = filmData.characters;
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
 
-	//Map each character URL to a promise
-	const characterPromises = characters.map(characterUrl => {
-		return new Promise((resolve, reject) => {
-			request(characterUrl, function (error, response, body) {
-				if (error) {
-					reject(error);
-				} else {
-					const characterData = JSON.parse(body);
-					resolve(characterData.name);
-				}
-			});
-		});
-	});
+  // Map each character URL to a promise
+  const characterPromises = characters.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, function (error, response, body) {
+        if (error) {
+          reject(error);
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
+    });
+  });
 
-	// Wait for all promises to resolve
-	Promise.all(characterPromises)
-	.then(characterNames => {
-		characterNames.forEach(name => console.log(name));
-	})
-	.catch(error => {
-		console.error(error);
-	});
+  // Wait for all promises to resolve
+  Promise.all(characterPromises)
+    .then(characterNames => {
+      characterNames.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
