ab7de2d999f7282e51d3ff28dc7564cdc024bf2a
davisjoseph6 <davisjoseph767@email.com>
2024-09-11 04:22:03 +0200

    update

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -59,5 +59,5 @@ int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
 		return (-1);
-	return (advanced_binary_recursive(array, 0, size -1, value));
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
