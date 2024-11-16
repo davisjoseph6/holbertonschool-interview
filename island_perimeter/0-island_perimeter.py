70b39175a277e00ddad7c402812c06d613a4bdd5
davisjoseph6 <davisjoseph6@gmail.com>
2024-11-16 13:54:49 +0100

    updated

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Module for calculating the perimeter of an island in a grid.
-"""
+""" Module for calculating the perimeter of an island in a grid. """
 
 
 def island_perimeter(grid):
