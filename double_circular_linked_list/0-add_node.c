9fff34df0427b3c3a80431aa33cd4b4a46484ffd
davisjoseph6 <davisjoseph6@gmail.com>
2024-11-16 12:45:43 +0100

    updated

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -11,42 +11,43 @@
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new_node, *tail;
+	List *new_node, *tail;
 
-    if (!list || !str)
-        return (NULL);
+	if (!list || !str)
+		return (NULL);
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (!*list)
-    {
-        new_node->prev = new_node;
-        new_node->next = new_node;
-        *list = new_node;
-    }
-    else
-    {
-        tail = (*list)->prev;
-        new_node->next = *list;
-        new_node->prev = tail;
-        tail->next = new_node;
-        (*list)->prev = new_node;
-    }
+	if (!*list)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		tail = (*list)->prev;
+		new_node->next = *list;
+		new_node->prev = tail;
+		tail->next = new_node;
+		(*list)->prev = new_node;
+	}
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
- * add_node_begin - Adds a new node to the beginning of a double circular linked list.
+ * add_node_begin - Adds a new node to the beginning of a double circular
+ * linked list.
  * @list: Double pointer to the head of the list.
  * @str: String to copy into the new node.
  *
@@ -54,14 +55,13 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node;
+	List *new_node;
 
-    new_node = add_node_end(list, str);
-    if (!new_node)
-        return (NULL);
+	new_node = add_node_end(list, str);
+	if (!new_node)
+		return (NULL);
 
-    *list = new_node;
+	*list = new_node;
 
-    return (new_node);
+	return (new_node);
 }
-
