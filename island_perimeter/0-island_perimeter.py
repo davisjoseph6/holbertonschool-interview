c0eeaf69914d0973f30ea5a0b5922b7ef4f8b8ad
davisjoseph6 <davisjoseph6@gmail.com>
2024-11-16 14:03:36 +0100

    updated

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -22,14 +22,28 @@ def island_perimeter(grid):
     cols = len(grid[0]) if rows > 0 else 0
 
     for i in range(rows):
+        """
+        .
+        """
         for j in range(cols):
+            """
+            .
+            """
             if grid[i][j] == 1:
-                # Add 4 for each land cell
+                """
+                Add 4 for each land cell
+                """
                 perimeter += 4
                 # Subtract 2 for each adjacent land cell
-                if i > 0 and grid[i - 1][j] == 1:  # Check cell above
+                if i > 0 and grid[i - 1][j] == 1:
+                    """
+                    Check cell above
+                    """
                     perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:  # Check cell to the left
+                if j > 0 and grid[i][j - 1] == 1:
+                    """
+                    Check cell to the left
+                    """
                     perimeter -= 2
-    return perimeter
 
+    return perimeter
