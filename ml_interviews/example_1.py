58fcc9654d7eac464a6019c49bf390c42359b989
davisjoseph6 <davisjoseph767@gmail.com>
2025-01-24 17:02:29 +0100

    updated

---
+++ ml_interviews/example_1.py
@@ -0,0 +1,7 @@
+#!/user/bin/env python3
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.feature_extraction.text import 
