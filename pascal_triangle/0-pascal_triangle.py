6cb22af0b9b920ca6886440a41c4e3c4ee9914bc
davisjoseph6 <davisjoseph767@gmail.com>
2025-01-24 18:15:11 +0100

    updated

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    """Returns a list of lists representing the Pascal’s triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            # The first row is always [1]
+            triangle.append([1])
+        else:
+            # Build the new row based on the previous row
+            row = [1]
+            prev_row = triangle[-1]  # Get the last (previous) row
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+            triangle.append(row)
+
+    return triangle
+
