d3104d0aec8bd911c0fe8adc6bd4b97fa756daa8
davisjoseph6 <davisjoseph767@gmail.com>
2025-01-24 18:44:13 +0100

    updated

--- substring/substring.h
+++ substring/substring.h
@@ -5,17 +5,18 @@
 #include <stdio.h>
 
 /**
- * find_substring - finds all the possible substrings containing a list of words
+ * find_substring - finds all possible substrings containing a list of words
  * @s: string to scan
- * @words: array of words (all same length)
+ * @words: array of words (all of the same length)
  * @nb_words: number of elements in @words
  * @n: address at which to store the number of elements in the returned array
  *
- * Description: A valid substring is the concatenation of each word in @words
- * exactly once, without any intervening characters, in any order.
+ * Description:
+ * A valid substring is the concatenation of each word in @words exactly
+ * once, without any intervening characters, in any order.
  *
  * Return: an allocated array of indices in @s where such a substring starts,
- * or NULL if no valid substring is found.
+ * or NULL if no valid substring is found
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
